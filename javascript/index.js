da330538fb97eadfce557faf12961f1a999d0199
David Rodriguez <rdavid1986@outlook.com>
2022-08-30 18:35:17 -0300

    simulador de cuotas de tarjeta de credito

--- javascript/index.js
+++ javascript/index.js
@@ -22,95 +22,93 @@ let impuestoPais = 0.30;
 let iva = 0.21;
 let impuestoSellos = 0.12;
 
-const bienvenido = alert("Bienvenido al simulador de cuotas de tajeta de credito");
-    let deseaUtilizar = prompt("Desea utilizar nuestro simulador?");
+let bienvenido = alert("Bienvenido al simulador de cuotas de tajeta de credito");
+const deseaUtilizar = prompt("Desea utilizar nuestro simulador?");
 
-    if (deseaUtilizar == "si") {
-        while (deseaUtilizar == "si") {
-            valorProducto = Number(prompt("Cuanto cuesta el producto?"));
+if (deseaUtilizar == "si") {
+    
+    let valorProducto
+    
+    do{
 
-            var ciudad = prompt("Eres de CABA?");
+    valorProducto = parseInt(prompt("Cuanto cuesta el producto?"));
 
-            let calculoCuota = ( valorProducto, cantidadCuotas, impuestopais, impuestopaiscaba, sellos,iva) => {
-            const precioProductoCuotas = valorProducto + (valorProducto * cantidadCuotas);
+        if (isNaN(valorProducto)){
 
-            let pagoTotalCuotas = precioProductoCuotas + (precioProductoCuotas * impuestopais) + (precioProductoCuotas * sellos) + (precioProductoCuotas * iva);
+            alert("El valor ingresado debe ser un valor numerico")
 
-            if (ciudad === "si") {
-                pagoTotalCuotas = precioProductoCuotas + (precioProductoCuotas * impuestopaiscaba) + (precioProductoCuotas * sellos) + (precioProductoCuotas * iva);
-
-                alert(
-                    "Con los impueston agregados tu producto ahora vale $" +
-                    pagoTotalCuotas
-                );
-            } else {
-                pagoTotalCuotas;
-
-                alert(
-                    "Con los impueston agregados tu producto ahora vale $" +
-                    pagoTotalCuotas
-                );
-            }
-            return pagoTotalCuotas;
-            };
-
-            function opcionesCuotas() {
-                opciones = Number(prompt("En cuantas cuotas quieres simular?\n En 3 cuotas \n En 6 cuotas \n En 12 cuotas\n En 18 cuotas\n En 24 cuotas"));
-                    switch (opciones) {
-                        case 3:
-                            alert("Elegiste calcular tu producto de $" + valorProducto + " en 3 cuotas");
-                            return (calculoCuota(valorProducto, tresCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 3);
-                        case 6:
-                            alert("Elegiste calcular tu producto en 6 cuotas");
-                            return (calculoCuota(valorProducto, seisCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 6);
-                        case 12:
-                            alert("Elegiste calcular tu producto en 12 cuotas");
-                            return (calculoCuota(valorProducto, doceCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva ) / 12);
-                        case 18:
-                            alert("Elegiste calcular tu producto en 18 cuotas");
-                            return (calculoCuota(valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva ) / 18 );
-                        case 24:
-                            alert("Elegiste calcular tu producto en 24 cuotas");
-                            return (calculoCuota( valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva) / 24);
-                        default:
-                            opciones = Number(prompt("En cuantas cuotas quieres simular?\n En 3 cuotas \n En 6 cuotas \n En 12 cuotas\n En 18 cuotas\n En 24 cuotas"));
-                    switch (opciones) {
-                        case 3:
-                            alert("Elegiste calcular tu producto de $" + valorProducto + " en 3 cuotas");
-                            return (calculoCuota(valorProducto, tresCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 3);
-                        case 6:
-                            alert("Elegiste calcular tu producto en 6 cuotas");
-                            return (calculoCuota(valorProducto, seisCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 6);
-                        case 12:
-                            alert("Elegiste calcular tu producto en 12 cuotas");
-                            return (calculoCuota(valorProducto, doceCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva ) / 12);
-                        case 18:
-                            alert("Elegiste calcular tu producto en 18 cuotas");
-                            return (calculoCuota(valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva ) / 18 );
-                        case 24:
-                            alert("Elegiste calcular tu producto en 24 cuotas");
-                            return (calculoCuota( valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva) / 24);
-                        default:
-                            alert("seleccione una opcion valida");
-                            opciones = Number(prompt("En cuantas cuotas quieres simular?\n En 3 cuotas \n En 6 cuotas \n En 12 cuotas\n En 18 cuotas\n En 24 cuotas"));
-                    }
-                    }
-
-
-            }
-
-            const pagoTotal = opcionesCuotas();
-            alert("La simulacion da como resultado " + opciones + " cuotas de " + "$" + pagoTotal + " cada una");
-            let volverAUtilizar = prompt("Desea volver a utilizar el simulador?");
-            if (volverAUtilizar == "no" || volverAUtilizar != "si") {
-            alert("Gracias por usar nuestro simulador de cuotas de tarjeta de credito ");
-            break;
-            } else if (volverAUtilizar == "si") {
-            deseaUtilizar == "si";
-            }
         }
-    } else if (deseaUtilizar != "si" && deseaUtilizar == "no") {
+
+    }while(isNaN(valorProducto)) 
+
+    var ciudad = prompt("Eres de CABA?");
+
+    let calculoCuota = ( valorProducto, cantidadCuotas, impuestopais, impuestopaiscaba, sellos,iva) => {
+    const precioProductoCuotas = valorProducto + (valorProducto * cantidadCuotas);
+
+        let pagoTotalCuotas = precioProductoCuotas + (precioProductoCuotas * impuestopais) + (precioProductoCuotas * sellos) + (precioProductoCuotas * iva);
+
+        if (ciudad === "si") {
+            pagoTotalCuotas = precioProductoCuotas + (precioProductoCuotas * impuestopaiscaba) + (precioProductoCuotas * sellos) + (precioProductoCuotas * iva);
+
+            alert(
+                "Con los impuestos agregados tu producto ahora vale $" +
+                pagoTotalCuotas
+            );
+
+        } else {
+            pagoTotalCuotas;
+
+            alert(
+                "Con los impuestos agregados tu producto ahora vale $" +
+                pagoTotalCuotas
+            );
+            
+        }
+        return pagoTotalCuotas;
+    };
+
+    function opcionesCuotas() {
+        
+        opciones = Number(prompt("En cuantas cuotas quieres simular?\n 3 cuotas \n 6 cuotas \n 12 cuotas\n 18 cuotas\n 24 cuotas"));
+        
+        switch (opciones) {
+        case 3:
+            alert("Elegiste calcular tu producto de $" + valorProducto + " en 3 cuotas");
+            return (calculoCuota(valorProducto, tresCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 3);
+        case 6:
+            alert("Elegiste calcular tu producto en 6 cuotas");
+            return (calculoCuota(valorProducto, seisCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 6);
+        case 12:
+            alert("Elegiste calcular tu producto en 12 cuotas");
+            return (calculoCuota(valorProducto, doceCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva ) / 12);
+        case 18:
+            alert("Elegiste calcular tu producto en 18 cuotas");
+            return (calculoCuota(valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva ) / 18 );
+        case 24:
+            alert("Elegiste calcular tu producto en 24 cuotas");
+            return (calculoCuota( valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva) / 24);
+        default:
+            alert ("No indico una opcion valida, vuelva a iniciar el simulador por favor")
+            return
+        }
+
+    }   
+        const pagoTotal = opcionesCuotas();
+        if (pagoTotal == undefined) {
+
+                alert("Consejo para obtener un resultado vuelva a intentarlo e ingrese la cantidad de cuotas a simular. Gracias por usar nuestro simulador ");
+
+            }else{
+
+                alert("La simulacion da como resultado " + opciones + " cuotas de " + "$" + pagoTotal + " cada una");
+
+                alert("Gracias por usar nuestro simulador de cuotas de tarjeta de credito ");
+            } 
+    
+    
+}else if (deseaUtilizar != "si" && deseaUtilizar == "no") {
     alert("Gracias");
-    }
+}
 
 
