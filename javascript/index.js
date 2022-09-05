9b75901e0eecdb40c2ad5d0f6140c72b7f82255c
David Rodriguez <rdavid1986@outlook.com>
2022-09-05 20:58:18 -0300

    simulador de reservas

--- javascript/index.js
+++ javascript/index.js
@@ -10,105 +10,66 @@
 
 // simulador de cuotas
 
-const impuestosPorPagoEnCuotas = 0.45
-const tresCuotas = 0.1350;
-const seisCuotas = 0.2150;
-const nueveCuotas = 0.30;
-const doceCuotas = 0.36;
-const dieciOchoCuotas = 0.46;
-const veintiCuatro = 0.52;
-let impuestoPaisCaba = 0.08;
-let impuestoPais = 0.30;
-let iva = 0.21;
-let impuestoSellos = 0.12;
+reservas = [];
 
-let bienvenido = alert("Bienvenido al simulador de cuotas de tajeta de credito");
-const deseaUtilizar = prompt("Desea utilizar nuestro simulador?");
+console.log(reservas);
 
-if (deseaUtilizar == "si") {
-    
-    let valorProducto
-    
-    do{
-
-    valorProducto = parseInt(prompt("Cuanto cuesta el producto?"));
-
-        if (isNaN(valorProducto)){
-
-            alert("El valor ingresado debe ser un valor numerico")
-
-        }
-
-    }while(isNaN(valorProducto)) 
-
-    var ciudad = prompt("Eres de CABA?");
-
-    let calculoCuota = ( valorProducto, cantidadCuotas, impuestopais, impuestopaiscaba, sellos,iva) => {
-    const precioProductoCuotas = valorProducto + (valorProducto * cantidadCuotas);
-
-        let pagoTotalCuotas = precioProductoCuotas + (precioProductoCuotas * impuestopais) + (precioProductoCuotas * sellos) + (precioProductoCuotas * iva);
-
-        if (ciudad === "si") {
-            pagoTotalCuotas = precioProductoCuotas + (precioProductoCuotas * impuestopaiscaba) + (precioProductoCuotas * sellos) + (precioProductoCuotas * iva);
-
-            alert(
-                "Con los impuestos agregados tu producto ahora vale $" +
-                pagoTotalCuotas
-            );
-
-        } else {
-            pagoTotalCuotas;
-
-            alert(
-                "Con los impuestos agregados tu producto ahora vale $" +
-                pagoTotalCuotas
-            );
-            
-        }
-        return pagoTotalCuotas;
-    };
-
-    function opcionesCuotas() {
-        
-        opciones = Number(prompt("En cuantas cuotas quieres simular?\n 3 cuotas \n 6 cuotas \n 12 cuotas\n 18 cuotas\n 24 cuotas"));
-        
-        switch (opciones) {
-        case 3:
-            alert("Elegiste calcular tu producto de $" + valorProducto + " en 3 cuotas");
-            return (calculoCuota(valorProducto, tresCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 3);
-        case 6:
-            alert("Elegiste calcular tu producto en 6 cuotas");
-            return (calculoCuota(valorProducto, seisCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva) / 6);
-        case 12:
-            alert("Elegiste calcular tu producto en 12 cuotas");
-            return (calculoCuota(valorProducto, doceCuotas, impuestoPais,impuestoPaisCaba, impuestoSellos, iva ) / 12);
-        case 18:
-            alert("Elegiste calcular tu producto en 18 cuotas");
-            return (calculoCuota(valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva ) / 18 );
-        case 24:
-            alert("Elegiste calcular tu producto en 24 cuotas");
-            return (calculoCuota( valorProducto, dieciOchoCuotas,impuestoPais, impuestoPaisCaba, impuestoSellos, iva) / 24);
-        default:
-            alert ("No indico una opcion valida, vuelva a iniciar el simulador por favor")
-            return
-        }
-
-    }   
-        const pagoTotal = opcionesCuotas();
-        if (pagoTotal == undefined) {
-
-                alert("Consejo para obtener un resultado vuelva a intentarlo e ingrese la cantidad de cuotas a simular. Gracias por usar nuestro simulador ");
-
-            }else{
-
-                alert("La simulacion da como resultado " + opciones + " cuotas de " + "$" + pagoTotal + " cada una");
-
-                alert("Gracias por usar nuestro simulador de cuotas de tarjeta de credito ");
-            } 
-    
-    
-}else if (deseaUtilizar != "si" && deseaUtilizar == "no") {
-    alert("Gracias");
+function Persona(nombre,apellido,dni,horario){
+    this.nombre =nombre,
+    this.apellido= apellido,
+    this.dni = dni,
+    this.horario = horario
 }
 
+option = prompt("Ingrese una opcion: \n1.hacer reserva \n2.ver tu reserva \n3.Cancelar reserva \n0. salir");
+while (option != "1" && option != "2" && option != "3" && option != "0"){ 
+    alert("debes agregar una opcion valida")
+    option = prompt("Ingrese una opcion: \n1.hacer reserva \n2.ver tu reserva \n3.Cancelar reserva \n0. salir")
+}
+while (option != "0"){
+    switch(option) {
+            case "1":
+                nombre = prompt("Ingrese su nombre");
+                apellido = prompt("Ingrese su apellido");
+                dni = prompt("Ingrese su dni");
+                horario = prompt("Elija el horario:\n 1. 7:00hs \n 2. 7:10hs \n 3.7:20hs \n 4.7:30hs");
+                while (horario != "1" && horario != "2" && horario != "3" &&horario != "4"){
+                    alert("ingrese una opcion correcta");
+                    horario = prompt("Elija el horario:\n 1. 7:00hs \n 2. 7:10hs \n 3.7:20hs \n 4.7:30hs");
+                }switch(horario){
+                    case "1":
+                        horario = "7:00hs";
+                        break
+                    case "2":
+                        horario = "7:10hs";
+                        break
+                    case "3":
+                        horario = "7:20hs";
+                        break;
+                    case "4":
+                        horario = "7:30hs";
+                        break
+                }
+                reservas.push(new Persona(nombre,apellido,dni,horario));
+                break
+            case "2":
+                const buscarDni = prompt("que dni buscas....");
+                const paso = reservas.find(elemento =>{
+                    return elemento.dni === buscarDni;
+                });
+                
+                alert("nombre: " + paso.nombre +"\n" + "apellido: "+paso.apellido + "\n" + "dni: " + paso.dni + "\n" + "cita alas: " + paso.horario);
+                console.log("nombre: " + paso.nombre + "\n" + "apellido: " + paso.apellido  + "\n" + "dni: " + paso.dni + "\n" + "cita alas: " + paso.horario);
+                break;
+                
+            case "3": 
+                reservaEliminar = prompt("ingrese el dni de la reserva a elminar");
 
+                const index = reservas.findIndex( x => x.dni === reservaEliminar );
+                alert("Su reserva fue cancelada");
+
+                reservas.splice( index, 1 );
+                console.log( reservas );
+
+        }option = prompt("Ingrese una opcion: \n1.hacer reserva \n2.ver tu reserva \n3.Cancelar reserva \n0. salir");
+}alert("cerrando programa")
