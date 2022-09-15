619da202388d787ee675afa9f2b6bad598ff419e
David Rodriguez <rdavid1986@outlook.com>
2022-09-15 19:09:50 -0300

    funcionando todo con input y cambios en el dom, con una accion enter que cambia el color de background de input nombre a color rojo

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,20 @@
+//reserva precargada
+
 const david = {
     nombre: "david",
     apellido:"rodriguez",
     dni: 1,
     dia: "lunes"
 }
+//Array de reservas
+
 reservas = [];
+//Push de reserva precargada
+
 reservas.push(david);
 console.log(reservas);
+
+//funcion constructora de reservas 
 
 class Persona {
     constructor(nombre, apellido, dni, dia) {
@@ -17,7 +25,7 @@ class Persona {
     }
 }
 
-//Funcion para capturar los datos de los inputs
+//funcion crear reserva
 
 function captura() {
     const nombreInput = document.getElementById("nombre").value;
@@ -44,16 +52,20 @@ function captura() {
     const nuevoUsuario = new Persona(nombreInput, apellidoInput, dni2, dia);
     reservas.push(nuevoUsuario);
     let section__div = document.getElementById("section__text");
-    section__div.innerHTML = "Su reserva fue realizada con exito";
+    section__div.innerHTML = "Su reserva fue realizada con exito" + "<br>" + "nombre: " + nuevoUsuario.nombre + "<br>" + "apellido: " + nuevoUsuario.apellido + "<br>"  + "dni: " + nuevoUsuario.dni + "<br>"  + "dia: " + nuevoUsuario.dia;
+
+    
     
     console.table(nuevoUsuario);
 }
 
 console.table(reservas);
 
+//funcion de buscar reserva
+
 function buscar(){
 
-    const buscarDni = document.getElementById("dniInput").value;
+    const buscarDni = document.getElementById("dniInput2").value;
     console.log(buscarDni);
     const filtrarDni = reservas.filter(reserva => reserva.dni == buscarDni );
     
@@ -70,6 +82,8 @@ function buscar(){
         filtrarDni.forEach(filtrarDni => section__div.innerHTML ="Usted tiene una reserva" + "<br>" + "nombre: " + filtrarDni.nombre + "<br>" + "apellido: "+ filtrarDni.apellido + "<br>"  + "dni: " + filtrarDni.dni + "<br>"  + "dia: " + filtrarDni.dia); 
     }
 }
+
+//funcion de cancelar reserva
 function cancelar(){
 
     reservaEliminar = document.getElementById("dniInput").value;
@@ -85,57 +99,93 @@ function cancelar(){
 }
 
 //botones de esconder
+//Boton reservar
+
 const btnEsconder = document.getElementById("btnEsconder");
 const esconderReservar = document.getElementById("esconderReservar");
 btnEsconder.addEventListener("click", toggleReservar);
-
 function toggleReservar() {
     esconderReservar.classList.toggle("mostrarReservar");
 
-
     if(esconderReservar.classList.contains("mostrarReservar")){
-        btnEsconder.innerHTML = "esconder Reservar"
+        btnEsconder.innerHTML = "Esconder Reservar"
         let section__h1 = document.getElementById("section__h1");
         section__h1.innerHTML = "Ingrese sus datos para hacer su reserva por favor";
     }else{
-        btnEsconder.innerHTML = " menu hacer Reserva"
+        btnEsconder.innerHTML = " Menu hacer Reserva"
         let section__h1 = document.getElementById("section__h1");
         section__h1.innerHTML = "Bienvenidos a nuestro sistema de reservas";
     }
 }
+
+//boton ver reserva
+
 const btnEsconder2 = document.getElementById("btnEsconder2");
 const esconderVerReserva = document.getElementById("esconderVerReserva");
-btnEsconder2.addEventListener("click", toggleVerReservar);
-
-function toggleVerReservar() {
+btnEsconder2.addEventListener("click", toggleVerReserva);
+function toggleVerReserva() {
     esconderVerReserva.classList.toggle("mostrarVerReserva");
 
 
     if(esconderVerReserva.classList.contains("mostrarVerReserva")){
-        btnEsconder2.innerHTML = "esconder ver Reserva"
+        btnEsconder2.innerHTML = "Esconder ver Reserva"
         let section__h1 = document.getElementById("section__h1");
         section__h1.innerHTML = "Ingrese su DNI para ver su reserva por favor";
     }else{
-        btnEsconder2.innerHTML = " menu ver su Reserva"
+        btnEsconder2.innerHTML = " Menu ver su Reserva"
         let section__h1 = document.getElementById("section__h1");
         section__h1.innerHTML = "Bienvenidos a nuestro sistema de reservas";
     }
 }
+
+//boton cancelar reserva
+
 const btnEsconder3 = document.getElementById("btnEsconder3");
 const esconderCancelar = document.getElementById("esconderCancelar");
 btnEsconder3.addEventListener("click", toggleCancelar);
-
 function toggleCancelar() {
     esconderCancelar.classList.toggle("mostrarCancelar");
 
 
     if(esconderCancelar.classList.contains("mostrarCancelar")){
-        btnEsconder3.innerHTML = "esconder cancelar su reserva"
+        btnEsconder3.innerHTML = "Esconder cancelar su reserva"
         let section__h1 = document.getElementById("section__h1");
         section__h1.innerHTML = "Ingrese su DNI para cancelar su reserva por favor";
     }else{
-        btnEsconder3.innerHTML = " menu cancelar su reserva"
+        btnEsconder3.innerHTML = " Menu cancelar su reserva"
         let section__h1 = document.getElementById("section__h1");
         section__h1.innerHTML = "Bienvenidos a nuestro sistema de reservas";
     }
 }
+
+/* const presionaEnter = document.getElementById("nombre");
+presionaEnter.addEventListener("keydown", presionar);
+
+
+addEventListener("keydown", (e) => {
+    if(e.keycode === 13 ) {
+        console.log(e.target.value);
+    }
+})
+function presionar() {
+    nombre.addEventListener("keydown", myScript);
+} */
+/* 
+const inputEnter = document.getElementById("nombre")
+inputEnter.addEventListener("keydown", presionaEnter);
+
+function presionaEnter(e) {
+    if (e.keycode === 13) {
+
+        document.getElementById("nombre").style.backgroundColor = "red";
+    }
+} */
+const inputEnter = document.getElementById('nombre');
+inputEnter.addEventListener('keyup', function(e) {
+  const keycode = e.keyCode || e.which;
+  if (keycode == 13) {
+    inputEnter.value;
+    document.getElementById("nombre").style.backgroundColor = "red";
+    console.log("Enter!");
+  }
+});
